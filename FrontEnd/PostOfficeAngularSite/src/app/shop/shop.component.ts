4a454b8a57c456229e0ddc2caea726eaed4b7984
awrohan <awrohan@users.noreply.github.com>
2019-04-12 22:34:39 -0500

    added html, typescript to shop page

--- FrontEnd/PostOfficeAngularSite/src/app/shop/shop.component.ts
+++ FrontEnd/PostOfficeAngularSite/src/app/shop/shop.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+export interface Item {
+  title: string;
+  description: string;
+  image: string;
+  price: string;
+  quantity: number;
+}
 
 @Component({
   selector: 'app-shop',
@@ -11,5 +19,24 @@ export class ShopComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  items: Item[] = [
+    {title:       'Thingamajig',
+     description: '4 out of 5 experts agree: it does something',
+     image:       'assets/shop_images/box1.jpg',
+     price:       '$500'},
+    {title:       'Foobar',
+     description: 'It foos the bars',
+     image:       'assets/shop_images/box2.jpg',
+     price:       '$500'},
+    {title:       'Placeholder',
+     description: 'Lorem ipsum latin stuff blah blah',
+     image:       'assets/shop_images/box3.jpg',
+     price:       '$500'},
+    {title:       'Whocares',
+     description: 'Stop reading this',
+     image:       'assets/shop_images/box4.jpg',
+     price:       '$500'},
+  ];
 
 }
